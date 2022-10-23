fbcc576a7bb2be2a5fbd0cadd8fee392aade715b
PavloKuK <90425406+PavloKuK@users.noreply.github.com>
2022-10-23 11:55:55 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,22 +1,19 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import java.awt.GridLayout;
-import java.awt.Color;
-import java.util.Random;
-import javax.swing.JFrame; //imports JFrame library
-import javax.swing.JButton; //imports JButton library
+import java.awt.*;
+
+import javax.swing.*;
 import java.awt.Color;
 import java.awt.GridLayout; //imports GridLayout library
+import java.util.Random;
+import java.awt.FlowLayout;
+
 
 public class Main {
-
-    static JFrame frame = new JFrame("Sprint 1"); // creates frame
+    static JFrame frame = new JFrame(); // creates frame
     static JButton[][] grid; // names the grid of buttons
 
     static Random random = new Random();
 
-    public static void Grid(int width, int length) { // constructor
+    public static Component Grid(int width, int length, int eagles, int giants) { // constructor
         frame.setLayout(new GridLayout(width, length)); // set layout
         grid = new JButton[width][length]; // allocate the size of grid
         int rand;
@@ -25,112 +22,128 @@ public class Main {
                 grid[x][y] = new JButton(); // creates new button
                 frame.add(grid[x][y]); // adds button to grid
 
-                rand = random.nextInt(4);
+                rand = random.nextInt(100);
 
-                switch (rand) {
-                    case 0:
-                        grid[x][y].setBackground(Color.black);
-                        break;
+                if ((0 < rand) && (rand < eagles)) {
+                    grid[x][y].setBackground(Color.green);
 
-                    case 1:
-                        grid[x][y].setBackground(Color.red);
-                        break;
+                } else if ((eagles < rand) && (rand < eagles +giants)) {
+                    grid[x][y].setBackground(Color.black);
 
-                    case 2:
-                        grid[x][y].setBackground(Color.gray);
-                        break;
-                    case 3:
-                        grid[x][y].setBackground(Color.white);
-                        break;
                 }
             }
-
         }
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack(); // sets appropriate size for frame
         frame.setVisible(true); // makes frame visible
+        return null;
     }
 
     public static void main(String[] args) {
+        //creates frame
+        JFrame frame = new JFrame("Sprint 1");
+        frame.setBounds(100, 100, 645, 470);
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
 
-        Grid(50, 50);
+        //creates label for width & text field and adds to panel
+        JLabel widthLabel = new JLabel("Enter width for grid: ");
+        JTextField widthField = new JTextField(10);
+        panel.add(widthLabel);
+        panel.add(widthField);
 
-        int black = 0;
-        int red = 0;
-        int gray = 0;
-        int white = 0;
+        //creates label for width & text field and adds to panel
+        JLabel heightLabel = new JLabel("Enter height for grid: ");
+        JTextField heightField = new JTextField(10);
+        panel.add(heightLabel);
+        panel.add(heightField);
 
-        for(int i = 0; i < 50; i++) {
-            for(int j = 0; j < 50; j++) {
-                if(grid[j][i].getBackground() == Color.black) {
-                    black++;
-                } else if (grid[j][i].getBackground() == Color.red) {
-                    red++;
-                } else if (grid[j][i].getBackground() == Color.gray) {
-                    gray++;
-                } else if (grid[j][i].getBackground() == Color.white) {
-                    white++;
+        //creates population density for Eagles
+        JLabel eagleDensityLabel = new JLabel("Enter density for eagles: ");
+        JTextField eaglesField = new JTextField(10);
+        panel.add(eagleDensityLabel);
+        panel.add(eaglesField);
+
+        //creates population density for Giants
+        JLabel giantsDensityLabel = new JLabel("Enter density for giants: ");
+        JTextField giantsField = new JTextField(10);
+        panel.add(giantsDensityLabel);
+        panel.add(giantsField);
+
+        //creates stop button
+        JButton stopButton = new JButton();
+        stopButton.setText("Stop");
+        stopButton.addActionListener(e -> frame.dispose());
+        panel.add(stopButton);
+
+
+        //creates submit button
+        JButton submitButton = new JButton("Submit");
+        submitButton.addActionListener(e -> {
+            //gets text
+            String width = widthField.getText();
+            String height = heightField.getText();
+            String eagleDensity = eaglesField.getText();
+            String giantsDensity = giantsField.getText();
+
+            //Converts width and height from string to int
+            int Width;
+            int Height;
+            int Eagles;
+            int Giants;
+            try {
+                Width = Integer.parseInt(width);
+                Height = Integer.parseInt(height);
+                Eagles = Integer.parseInt(eagleDensity);
+                Giants = Integer.parseInt(giantsDensity);
+
+                //Creates Grid
+                Grid(Width, Height, Eagles, Giants);
+
+                //Calculates population density for teams
+                int green = 0;
+                int black = 0;
+                int blank = 0;
+
+                for(int y = 0; y < Height; y++) {
+                    for(int x = 0; x < Width; x++) {
+                        if(grid[x][y].getBackground() == Color.black) {
+                            black++;
+                        } else if (grid[x][y].getBackground() == Color.green) {
+                            green++;
+                        } else {
+                            blank++;
+                        }
+                    }
                 }
+
+                double blackPopulation = (double) black / (Height * Width) * 100;
+                double greenPopulation = (double) green / (Height * Width) * 100;
+                double blankPopulation = (double) blank / (Height * Width) * 100;
+
+                //Prints out population
+                System.out.println(blackPopulation + "\n" +
+                        greenPopulation + "\n" +
+                        blankPopulation + "\n\n\n");
+
+                System.out.println("Black population: " + black + "\n"
+                        + "Red population: " + green + "\n"
+                        + "Blank population: " + blank + "\n");
+
+            } catch (Exception E) {
+                System.out.println("Got an exception");
             }
-        }
 
-        double blackPopulation = (double) black / 2500 * 100;
-        double redPopulation = (double) red / 2500 * 100;
-        double grayPopulation = (double) gray / 2500 * 100;
-        double whitePopulation = (double) white / 2500 * 100;
+        });
+        panel.add(submitButton);
 
-        System.out.println(blackPopulation + "\n" +
-                redPopulation + "\n" +
-                grayPopulation + "\n" +
-                whitePopulation + "\n\n\n");
+        //adds a panel to the frame
+        frame.add(panel);
 
-        System.out.println("Black population: " + black + "\n"
-                + "Red population: " + red + "\n"
-                + "Gray population: " + gray + "\n"
-                + "White population: " + white);
+        //displays frame
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
     }
 }
-
-//import javax.swing.JFrame; //imports JFrame library
-//import javax.swing.JButton; //imports JButton library
-//import java.awt.Color;
-//import java.awt.GridLayout; //imports GridLayout library
-
-//public class Main {
-//
-//    public static void main(String[] args) {
-//        new Grid(50, 50);// makes new ButtonGrid with 2 parameters
-//    }
-//
-//    public static class Grid {
-//        JFrame frame = new JFrame(); // creates frame
-//        JButton[][] grid; // names the grid of buttons
-//
-//        Random random = new Random();
-//
-//        public Grid(int width, int length) { // constructor
-//            frame.setLayout(new GridLayout(width, length)); // set layout
-//            grid = new JButton[width][length]; // allocate the size of grid
-//            int rand;
-//            for (int y = 0; y < length; y++) {
-//                for (int x = 0; x < width; x++) {
-//                    grid[x][y] = new JButton(); // creates new button
-//                    frame.add(grid[x][y]); // adds button to grid
-//
-//                    rand = random.nextInt(4);
-//
-//                    switch (rand) {
-//                        case 0 -> grid[x][y].setBackground(Color.black);
-//                        case 1 -> grid[x][y].setBackground(Color.red);
-//                        case 2 -> grid[x][y].setBackground(Color.gray);
-//                        case 3 -> grid[x][y].setBackground(Color.white);
-//                    }
-//                }
-//
-//            }
-//            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//            frame.pack(); // sets appropriate size for frame
-//            frame.setVisible(true); // makes frame visible
-//        }
-//    }
-//}
